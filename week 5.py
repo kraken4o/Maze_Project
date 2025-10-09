4b15205a399e72432d2d7fc2b1840da00a106fde
alexa <alexamariekadiri@gmail.com>
2025-10-09 12:21:42 +0200

    Added a key to the room

---
+++ week 5.py
@@ -0,0 +1,28 @@
+students = ["Alice", "Bob", "Charlie", "Diana", "Ethan",
+            "Fiona", "George", "Hannah", "Ivan", "Julia",
+            "Kevin", "Fernando", "Michael", "Nina", "Graham"]
+math_scores =    [85, 67, 90, 78, 67, 92, 55, 73, 88, 60, 34, 82, 77, 69, 84]
+science_scores = [88, 72, 85, 80, 70, 91, 58, 75, 83, 65, 97, 78, 81, 72, 89]
+english_scores = [92, 60, 87, 75, 65, 94, 62, 79, 90, 35, 96, 85, 73, 70, 29]
+
+# [ [student1, math, science, english], [name2, math, science, english], [], ...]
+# step 1: student [0], math [0], science [0], english [0]
+# step 2: append the list to outer list
+# step 3: repeat step 1 and 2 for every student
+
+# method 1
+def combine_data(names,math,science,english):
+    pass
+list_names = []
+
+list_names.append( [ names[0], math[0], science[0], english[0]] )
+returnlist_names
+
+names_scores = combine_data(names,math_scores,science_scores,english_scores)
+
+
+
+
+
+combine_data(students,math_scores,science_scores,english_scores)
+
